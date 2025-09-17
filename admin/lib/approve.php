5322520c49b387e91a1adc209628f4ad3d464adc
Kavishka Adhikari <kavishkaadhikari0@gmail.com>
2025-09-18 01:41:08 +0530

    add approve deapprove php file
    add approve deapprove php file

---
+++ admin/lib/approve.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include "../include/connection.php";
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $query = "UPDATE businessregistration SET approve='1' WHERE id='$id'";
+    if (mysqli_query($con, $query)) {
+        header("location:../pages/manage.php");
+        exit();
+    }
+}
+?>
